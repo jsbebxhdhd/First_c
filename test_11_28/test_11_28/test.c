f54c1ec43f879d18cb36ea85ef5b3e595b98cdc2
LuXinYe <2027192825@qq.com>
2022-11-29 21:37:36 +0800

    11_28

---
+++ test_11_28/test_11_28/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	//本质山是把“hello wqnd”字符串的首字符的地址存储在了ps中
+	/*char* ch = "hello wqnd";
+	printf("%c\n", *ch);
+	printf("%s\n", ch);*/
+
+
+	return 0;
+}
